6936328f23d13f0cbf439a5903ddb66674c6e01c
DhanyaRHegde <dhanya.rhegde@codeandtheory.com>
2024-07-18 13:03:59 +0530

    01-first-commit

---
+++ components/Header.tsx
@@ -0,0 +1,28 @@
+import Link from 'next/link'
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header>
+      <nav>
+        <div className="navbar justify-between bg-orange-900 ">
+          <Link href="/">Barista</Link>
+          <ul className="flex">
+            <li>
+              <Link className="btn btn-ghost rounded-btn" href="/cart">
+                Cart
+              </Link>
+            </li>
+            <li>
+              <Link className="btn btn-ghost rounded-btn" href="/signin">
+                Login
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </header>
+  )
+}
+
+export default Header
