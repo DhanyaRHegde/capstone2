49bbff5353163b92045a833e97d425f7608fc7ef
DhanyaRHegde <dhanya.rhegde@codeandtheory.com>
2024-07-30 17:07:28 +0530

    09-updated

--- app/(front)/cart/page.tsx
+++ app/(front)/cart/page.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import CartDetails from '../../../components/CartDetails'
-import withRedirect from '@/hoc/Redirect'
 
 export const metadata = {
   title: 'Shopping Cart',
--- app/(front)/checkout/page.tsx
+++ app/(front)/checkout/page.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import CheckoutDetails from '@/components/CheckoutDetails'
-import withNavigationProtection from '@/hoc/Redirect'
 
 export const metadata = {
   title: 'Checkout',
